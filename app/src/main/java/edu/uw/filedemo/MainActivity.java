1607afc7e0f83d8c37b129a470368f2edb48b551
Joel Ross <joelross@uw.edu>
2017-04-26 19:07:31 -0700

    Reset to lecture starter code

--- app/src/main/java/edu/uw/filedemo/MainActivity.java
+++ app/src/main/java/edu/uw/filedemo/MainActivity.java
@@ -1,14 +1,8 @@
 package edu.uw.filedemo;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -19,27 +13,12 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
-import static android.os.Environment.getExternalStoragePublicDirectory;
-
-//TODO: Refactor to avoid repeated code
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
 
     private static final String FILE_NAME = "myFile.txt";
-
-    private static final int WRITE_REQUEST_CODE = 1;
-
+    
     private EditText textEntry; //save reference for quick access
     private RadioButton externalButton; //save reference for quick access
 
@@ -56,68 +35,22 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Saving file...");
 
         if(externalButton.isChecked()){ //external storage
-            if(isExternalStorageWritable()){
-                //check permission to write
-                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-                    //have permission, can go ahead and do stuff
-                    Log.v(TAG, "Permission granted!");
-                    saveToExternalFile();
-                }
-                else { //if we're missing permission.
-                    Log.v(TAG, "Permission denied!");
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_REQUEST_CODE);
-                }
-            }
+
+            saveToExternalFile(); //helper method
+
         }
         else { //internal storage
-            try {
-                FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
 
-                PrintWriter out = new PrintWriter(fos);
-                out.println(textEntry.getText().toString());
-                out.close();
 
-            }catch (IOException ioe){
-                Log.d(TAG, Log.getStackTraceString(ioe));
-            }
         }
     }
 
     //actually write to the file
     private void saveToExternalFile(){
-        try {
-            File dir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-            if (!dir.exists()) {
-                dir.mkdirs(); //make Documents directory if doesn't otherwise exist
-            }
-            File file = new File(dir, FILE_NAME);
-            Log.v(TAG, "Saving to  " + file.getAbsolutePath());
 
-            PrintWriter out = new PrintWriter(new FileWriter(file, true));
-            out.println(textEntry.getText().toString());
-            out.close();
-
-        } catch (IOException ioe) {
-            Log.d(TAG, Log.getStackTraceString(ioe));
-        }
     }
 
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch(requestCode){
-            case WRITE_REQUEST_CODE: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    //now we have permissions!
-                    saveToExternalFile(); //do the work
-                }
-                break;
-            }
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
 
     public void loadFile(View v){
         Log.v(TAG, "Loading file...");
@@ -125,53 +58,10 @@ public class MainActivity extends AppCompatActivity {
         textDisplay.setText(""); //clear initially
 
         if(externalButton.isChecked()){ //external storage
-            if(externalButton.isChecked()) { //external storage
-                if (isExternalStorageWritable()) {
-                    try {
-                        File dir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-                        File file = new File(dir, FILE_NAME);
-                        BufferedReader reader = new BufferedReader(new FileReader(file));
-                        StringBuilder text = new StringBuilder();
 
-                        //read the file
-                        String line = reader.readLine();
-                        while (line != null) {
-                            text.append(line + "\n");
-                            line = reader.readLine();
-                        }
-
-                        textDisplay.setText(text.toString());
-
-                        reader.close();
-
-                    } catch (IOException ioe) {
-                        Log.d(TAG, Log.getStackTraceString(ioe));
-                    }
-                }
-            }
         }
         else { //internal storage
 
-            try {
-                FileInputStream fis = openFileInput(FILE_NAME);
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-                StringBuilder text = new StringBuilder();
-
-                //read the file
-                String line = reader.readLine();
-                while (line != null) {
-                    text.append(line + "\n");
-                    line = reader.readLine();
-                }
-
-                textDisplay.setText(text.toString());
-
-                reader.close();
-
-            } catch (IOException ioe) {
-                Log.d(TAG, Log.getStackTraceString(ioe));
-            }
         }
     }
 
@@ -181,39 +71,14 @@ public class MainActivity extends AppCompatActivity {
 
         Uri fileUri = null;
         if(externalButton.isChecked()){ //external storage
-            //File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS); //
-            File dir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-            File file = new File(dir, FILE_NAME);
-            fileUri = Uri.fromFile(file);
+
         }
         else { //internal storage
-            File dir = getFilesDir();
-            File file = new File(dir, FILE_NAME);
-            fileUri = Uri.fromFile(file);
+
         }
 
-        if(fileUri != null) {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_STREAM, fileUri); //external app needs permission to this (public) Uri
-
-            Intent chooser = Intent.createChooser(intent, "Share File");
-            //check that there is at least one option
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(chooser);
-            }
-        }
     }
 
-
-    //helper method to confirm external storage is around
-    public static boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
 
 
     @Override
--- app/src/main/java/edu/uw/filedemo/PhotoActivity.java
+++ app/src/main/java/edu/uw/filedemo/PhotoActivity.java
@@ -1,20 +1,13 @@
 package edu.uw.filedemo;
 
 import android.content.Intent;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -22,8 +15,6 @@ public class PhotoActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private Uri pictureFileUri = null; //for saving
-    private Uri mediaStoreUri = null; //for sharing
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,47 +29,18 @@ public class PhotoActivity extends AppCompatActivity {
         Log.v(TAG, "Taking picture...");
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-
-            File file = null;
-            try {
-                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //include timestamp
-
-                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                file = new File(dir, "PIC_"+timestamp+".jpg");
-                boolean created = file.createNewFile(); //actually make the file!
-                Log.v(TAG, "File created: "+created);
-
-            } catch (IOException ioe) {
-                Log.d(TAG, Log.getStackTraceString(ioe));
-            }
-
-            if(file != null){ //make sure we can save the file!
-                pictureFileUri = Uri.fromFile(file);
-                Log.v(TAG, "Uri: "+pictureFileUri);
-
-                //for sharing the media (produces a Uri the Messenger has permissions for)
-                MediaScannerConnection.scanFile(this, new String[] { file.toString() }, null,
-                        new MediaScannerConnection.OnScanCompletedListener() {
-                            public void onScanCompleted(String path, Uri uri) {
-                                mediaStoreUri = uri;
-                                Log.v(TAG, "MediaStore Uri: "+uri);
-                            }
-                        });
-
-
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureFileUri);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
             ImageView imageView = (ImageView)findViewById(R.id.imgThumbnail);
-            imageView.setImageURI(pictureFileUri);
+            imageView.setImageBitmap(imageBitmap);
 
         }
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,15 +49,5 @@ public class PhotoActivity extends AppCompatActivity {
     public void sharePicture(View v){
         Log.v(TAG, "Sharing picture...");
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_STREAM, mediaStoreUri);
-        Log.v(TAG, "Uri: "+mediaStoreUri);
-
-        Intent chooser = Intent.createChooser(intent, "Share Picture"); //prompt user to pick
-        //check that there is at least one option
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(chooser);
-        }
     }
 }
